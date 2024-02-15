0ad89b70698f1ce46c2b00599db4a6e061faa0c7
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2024-02-16 01:44:02 +0330

    chore: signing key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,17 @@ android {
         }
     }
 
+    signingConfigs {
+        if(System.getenv("KEY_FILE_PATH") != null) {
+            create("release") {
+                storeFile = file(System.getenv("KEY_FILE_PATH"))
+                storePassword = System.getenv("KEY_FILE_PASSWORD")
+                keyAlias = System.getenv("KEY_ALIAS")
+                keyPassword = System.getenv("KEY_PASSWORD")
+            }
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
