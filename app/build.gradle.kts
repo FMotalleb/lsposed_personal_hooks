e78ca6e0b7547e8779f5c3273736e34be80618f9
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2024-02-16 03:06:12 +0330

    chore: versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "monster.moti.lsposed"
         minSdk = 33
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
